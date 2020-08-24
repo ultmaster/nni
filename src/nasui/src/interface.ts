bd0b1ccd08f0a2be1ea4456f55f2b90035e1656c
Yuge Zhang <scottyugochang@gmail.com>
2020-08-24 17:29:24 +0800

    Initiate interface

---
+++ src/nasui/src/interface.ts
@@ -0,0 +1,50 @@
+/**
+ * Node represents a single, primitive calculation on graph.
+ * Producing a new parameter / input, can also be thought of as an calculation.
+ * A node does not necessarily mean there is a tensor, the output of a node can be many things.
+ * A node accepts one or many ordered inputs, from other nodes.
+ */
+interface Node {
+    id: string;
+    inputs: string[];
+    name: string[]
+    op: string;
+    attrs: Map<string, any>;
+    parent: Cluster;
+}
+
+/**
+ * A cluster is linked with a set of nodes/sub-clusters.
+ * The inputs of a cluster is the union of all inputs in its set.
+ */
+interface Cluster {
+    id: string;
+    inputs: string[];
+}
+
+interface Edge {
+
+}
+
+interface Tag {
+    id: string;
+    associatedElements: string[];
+}
+
+interface Visualizer {
+
+}
+
+interface Frame {
+    graphNodes: Node[];
+    graphClusters: Cluster[];
+    graphEdges: Edge[];
+    tags: Tag[];
+    visualizers: Map<Tag, Visualizer>;
+}
+
+interface VisualizationConfig {
+    initialFrame: Frame;
+    frames: Frame[];
+
+}
